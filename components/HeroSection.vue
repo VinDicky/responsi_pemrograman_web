6194d6923ab7a1d230d035e27b52308827d4eea8
VinDicky <kevindicky11@students.amikom.ac.id>
2024-07-04 14:26:06 +0700

    Responsi PemWeb

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -9,10 +9,10 @@
               <div class="card">
                 <div class="card-body">
                   <div class="d-flex flex-column align-items-center text-center">
-                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+                    <img src="/assets/img/foto.jpg" alt="Admin" class="rounded-circle" width="150">
                     <div class="mt-3">
                       <h4>Kevin Dicky</h4>
-                      <p class="text-secondary mb-1">Frontend Development</p>
+                      <p class="text-secondary mb-1">Web Development</p>
                       <p class="text-muted font-size-sm">Sleman, YK</p>
                       <button class="btn btn-primary">Follow</button>
                       <button class="btn btn-outline-primary">Message</button>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
-    <HeroSection />
+    <HeroSection/>
+  </div>
+  <div>
+    <AboutSection/>
+  </div>
+  <div>
+    <NavbarSection/>
+  </div>
+  <div>
+    <ProjectsSection/>
+  </div>
+  <div>
+    <ContactSection/>
   </div>
 </template>
 
 <script>
 import HeroSection from '~/components/HeroSection.vue';
 import AboutSection from '~/components/AboutSection.vue';
+import SkillSection from '~/components/NavbarSection.vue';
 import ProjectsSection from '~/components/ProjectsSection.vue';
 import ContactSection from '~/components/ContactSection.vue';
+import NavbarSection from '../components/NavbarSection.vue';
+
 
 export default {
   components: {
